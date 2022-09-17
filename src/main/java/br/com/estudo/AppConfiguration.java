fb0df384a6b0a385f13f4ae461988d6a495f59c2
Paulo Renato Macêdo <prmacedoandrade@gmail.com>
2022-09-17 20:19:00 -0300

    Profile with annotation

--- src/main/java/br/com/estudo/AppConfiguration.java
+++ src/main/java/br/com/estudo/AppConfiguration.java
@@ -1,14 +1,21 @@
 package br.com.estudo;
 
+import org.springframework.boot.CommandLineRunner;
 import org.springframework.context.annotation.Bean;
-import org.springframework.context.annotation.Configuration;
 
-@Configuration
+@Development
 public class AppConfiguration {
 
+//    @Bean
+//    public String applicationName(){
+//        return "System name";
+//    }
+
     @Bean
-    public String applicationName(){
-        return "System name";
+    public CommandLineRunner execute(){
+        return args -> {
+            System.out.println("RUNNING DEVELOPMENT CONFIG");
+        };
     }
 
 }
